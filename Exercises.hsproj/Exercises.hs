1b6d02fa690ed7a6cd9cafb9a7c190d1a5a306c4
Marcus Kida <marcus@kida.io>
2016-03-03 16:05:43 +1100

    Add exercise descriptions

--- Exercises.hsproj/Exercises.hs
+++ Exercises.hsproj/Exercises.hs
@@ -1,9 +1,19 @@
 -- Chapter 1
+{-
+“ Define a function product that produces the product of a list of numbers, and show using your definition that product [2, 3, 4] = 24.”
+
+Excerpt From: Graham Hutton. “Programming in Haskell.” iBooks.
+-}
 pdct [] = 1
 pdct (x:xs) = x * pdct xs
 
+{-
+“How should the definition of the function qsort be modified so that it produces a reverse sorted version of a list?”
+
+Excerpt From: Graham Hutton. “Programming in Haskell.” iBooks. 
+-}
 qsortr [] = []
 qsortr (x:xs) = qsortr smaller ++ [x] ++ qsortr larger
                 where
                   smaller = [a | a<-xs, a>=x]
-                  larger = [b | b<-xs, b<x]
+                  larger = [b | b<-xs, b<x]s
