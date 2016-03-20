a1d3e1ecd72053979516de04079e77eec37dc61f
Marcus Kida <marcus@kida.io>
2016-03-20 18:49:30 +1100

    Add Chapter 3

---
+++ Exercises.hsproj/Chapter3.hs
@@ -0,0 +1,11 @@
+module Chapter3 where
+  
+{-
+“1.  What are the types of the following values?
+
+”
+
+Excerpt From: Graham Hutton. “Programming in Haskell.” iBooks. 
+-}
+
+pldr xs =  reverse xs  == xs
